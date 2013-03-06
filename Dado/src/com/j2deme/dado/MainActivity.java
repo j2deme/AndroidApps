1767342f0f625b7a232b0c7b128186ebb2187107
j2deme <j2deme@gmail.com>
2013-03-06 08:06:12 -0600

    Add initial release of Dado project

---
+++ Dado/src/com/j2deme/dado/MainActivity.java
@@ -0,0 +1,35 @@
+package com.j2deme.dado;
+
+import java.util.Random;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	private Button Dado;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Dado = (Button) findViewById(R.id.Dado);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+	
+	public void lanzar(View Button) {
+		int nextValue;
+		Random rand = new Random();
+    	nextValue = rand.nextInt(6) + 1;
+    	Dado.setText(""+nextValue);
+	  }
+
+}
