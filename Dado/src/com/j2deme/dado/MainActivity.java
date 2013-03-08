504649fa5d6f20c3929349f62d04cbad3ace4ca7
j2deme <j2deme@gmail.com>
2013-03-08 17:36:30 -0600

    Fix to show images on dice sides
    - Resources for images at different dpis were added.
    - Change of text was commented as it conflicts with image.
    - Added conditional to address the use setBackgroundDrawable on APIs lower than 16
    - Created function to get a random int from a range ti separate functionality

--- Dado/src/com/j2deme/dado/MainActivity.java
+++ Dado/src/com/j2deme/dado/MainActivity.java
@@ -2,9 +2,11 @@ package com.j2deme.dado;
 
 import java.util.Random;
 
-import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -26,8 +28,12 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
+	@SuppressWarnings("deprecation")
+	@SuppressLint("NewApi")
 	public void lanzar(View b) {
 		Resources res = getResources();
+		int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+		/* Para cambiar el texto
 		String[] numbers = {
 			res.getString(R.string.one),
 			res.getString(R.string.two),
@@ -36,10 +42,31 @@ public class MainActivity extends Activity {
 			res.getString(R.string.five),
 			res.getString(R.string.six)
 		};
-		int nextValue;
+		Dado.setText(numbers[nextValue-1]);
+		*/
+		
+		Drawable[] sides = {
+			res.getDrawable(R.drawable.dice_1),
+			res.getDrawable(R.drawable.dice_2),
+			res.getDrawable(R.drawable.dice_3),
+			res.getDrawable(R.drawable.dice_4),
+			res.getDrawable(R.drawable.dice_5),
+			res.getDrawable(R.drawable.dice_6),
+		};
+		
+    	Drawable drawable = sides[random()-1];
+		if (currentapiVersion >= android.os.Build.VERSION_CODES.JELLY_BEAN){
+			Dado.setBackground(drawable);
+		} else{
+			Dado.setBackgroundDrawable(drawable);
+		}
+	}
+	
+	public int random(){
+		int r;
 		Random rand = new Random();
-    	nextValue = rand.nextInt(6) + 1;
-    	Dado.setText(numbers[nextValue-1]);
-	  }
+    	r = rand.nextInt(6) + 1;
+    	return r;
+	}
 
 }
